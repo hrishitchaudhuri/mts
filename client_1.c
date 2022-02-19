0bc317ca87a6ca27b6f9e3708fe86f31f26ef333
Hrishit Chaudhuri <hrishitchaudhuri@gmail.com>
2022-02-19 09:32:40 +0530

    Init

---
+++ client_1.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<string.h>
+
+#define PORT 8080
+
+int main(int argc, char *argv[]) {
+    int socket_d, ch;
+    socket_d = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in server;
+
+    char message[1024];
+    char *end_msg = "END";
+
+    socket_d = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_d < 0) {
+        printf("Socket creation failed.\n");
+        return -1;
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &server.sin_addr) <= 0) {
+        printf("Invalid address.\n");
+        return -1;
+    }
+
+    if (connect(socket_d, (struct sockaddr *) &server, sizeof(server)) < 0) {
+        printf("Connection failed.\n");
+        return -1;
+    }
+
+    #if 0
+    printf("1. Send data.\n2. End connection.\n");
+    while (1) {
+        printf("Enter choice: ");
+        scanf("%d", &ch);
+
+        switch(ch) {
+            case 1:
+                printf("Enter message: ");
+                scanf(" %[^\n]s", message);
+                send(socket_d, message, strlen(message), 0);
+                break;
+
+            case 2:
+                printf("Terminating connection...\n");
+                send(socket_d, end_msg, strlen(end_msg), 0);
+                return 0;
+
+            default:
+                printf("[ERR] Invalid choice.\n");
+        }
+    }
+    #endif 
+
+    #if 1
+    end_msg = "aaaaaaaa";
+    // for (int i = 0; i < 100; i++)
+        send(socket_d, end_msg, strlen(end_msg) + 1, 0);
+
+    end_msg = "END";
+    send(socket_d, end_msg, strlen(end_msg) + 1, 0);
+    #endif
+
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,87 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<sys/socket.h>
+#include<stdlib.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<pthread.h>
+
+#define PORT 8080
+
+void *connection_handler(void *);
+
+int main(int argc, char *argv[]) {
+    int socket_d, socket_len, new_socket;
+    socket_d = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (socket_d == -1) {
+        perror("socket creation failed.");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in client, server;
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(PORT);
+
+    if (bind(socket_d, (struct sockaddr *) &server, sizeof(server)) < 0) {
+        perror("bind failed.");
+        exit(EXIT_FAILURE);
+    }
+
+    listen(socket_d, 3);
+
+    int *temp_socket;
+    socket_len = sizeof(struct sockaddr_in);
+    while (new_socket = accept(socket_d, (struct sockaddr *) &client, (socklen_t*) &socket_len)) {
+        pthread_t handler_t;
+        
+        temp_socket = malloc(1);
+        *temp_socket = new_socket;
+
+        if (pthread_create(&handler_t, NULL, connection_handler, (void *) temp_socket) < 0) {
+            perror("thread creation failed.");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("[INFO] Handler assigned.\n");
+    }
+
+    if (new_socket < 0) {
+        perror("accept failed.");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+void *connection_handler(void *socket_d) {
+    int socket = *(int *) socket_d;
+    int valread, i = 0;
+    char buffer[1024];
+    char ch;
+
+    bzero(buffer, 1024);
+
+    while ((valread = recv(socket, buffer, 1024, MSG_DONTWAIT)) > 0) {
+        if (strcmp(buffer, "END") == 0) {
+            printf("[INFO %d] Connection terminated by client.\n", socket);
+            free(socket_d);
+            return 0;
+        }
+        printf("[COMM FR: %d] %s\n", socket, buffer);
+        bzero(buffer, 1024);
+    }
+
+    if (valread == 0) {
+        printf("[INFO %d] Client disconnected.\n", socket);
+        fflush(stdout);
+    }
+
+    else if (valread == -1) {
+        perror("recv failed.");
+    }
+
+    free(socket_d);
+    return 0;
+}
